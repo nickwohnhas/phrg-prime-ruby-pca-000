9a45838996e074b1480cd090de9a400d02186a24
Nick Wohnhas <nicholas.wohnhas@powerhrg.com>
2018-05-11 11:08:02 -0400

    I built a method for determining if an integer is a prime number

--- prime.rb
+++ prime.rb
@@ -1,1 +1,10 @@
-# Add  code here!
+def prime?(num)
+  if (num > 2)
+  one_less = num - 1
+  !(2..one_less).to_a.any?{|divisor| num % divisor == 0}
+  elsif (num == 2)
+  true
+  else
+  false
+  end
+end
